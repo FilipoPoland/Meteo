e48c9f1a08ff9cab005a08695793fda3884add59
FilipoPoland <40774085+FilipoPoland@users.noreply.github.com>
2023-05-11 15:09:00 +0200

    Commit

---
+++ tKinter.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+des_q1 = ttk.Label(text='Proszę o podanie ilości akwenów, na której znajdzie się statek.')
+usr_q1 =
+
+
+root = tk.Tk()
+root.mainloop()
