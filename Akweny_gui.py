bdcb0a7b81acb082aa1cc35af989968ee8c6138f
FilipoPoland <40774085+FilipoPoland@users.noreply.github.com>
2023-05-18 19:46:46 +0200

    Commit

--- Akweny_gui.py
+++ Akweny_gui.py
@@ -1,35 +1,35 @@
 import tkinter
 
 
-def density(water_temp, salinity):
-    pu = (28.1522 - (0.0735 * float(water_temp)) - (0.00469 * (float(water_temp) ** 2)) +
-          (0.802 - (0.002 * float(water_temp))) * (float(salinity) - 35.0))
-    g = float(pu) / 1000 + 1
-    return g
+# def density(water_temp, salinity):
+#     pu = (28.1522 - (0.0735 * float(water_temp)) - (0.00469 * (float(water_temp) ** 2)) +
+#           (0.802 - (0.002 * float(water_temp))) * (float(salinity) - 35.0))
+#     g = float(pu) / 1000 + 1
+#     return g
 
 
-def draughts(densty_list, zanurzenie0):
-    draughts_list = []
-    for i in densty_list:
-        zanurzenie = ((float(densty_list[i])) / float(densty_list[i+1])) * float(zanurzenie0)
-        draughts_list.append(zanurzenie)
-        zanurzenie0 = zanurzenie
-    return draughts_list
+# def draughts(densty_list, zanurzenie0):
+#     draughts_list = []
+#     for i in densty_list:
+#         zanurzenie = ((float(densty_list[i])) / float(densty_list[i+1])) * float(zanurzenie0)
+#         draughts_list.append(zanurzenie)
+#         zanurzenie0 = zanurzenie
+#     return draughts_list
 
 
-def pol_usr(get_input):
-    fr_cptr = get_input.replace(',', '.')
-    fr_cptr = fr_cptr.replace(' ', '')
-    return fr_cptr
+# def pol_usr(get_input):
+#     fr_cptr = get_input.replace(',', '.')
+#     fr_cptr = fr_cptr.replace(' ', '')
+#     return fr_cptr
 
-
-def btnclck():
+def btnpress():
     counting()
 
+    # to mimo dodawania pól nie robi dla mnie nic przydatnego bo nie wiem jak odwołać się do tych pół
     # akwen nazwa
     lbl = tkinter.Label(text=f'Akwen {count-1}')
     # temperatura wody pole do danych
-    tw_input = tkinter.Entry(root, )
+    tw_input = tkinter.Entry(root, validate='key')
     # zasolenie pole danych
     zsl_input = tkinter.Entry(root, )
 
@@ -37,6 +37,7 @@ def btnclck():
     lbl.grid(column=1, row=count)
     # temperatura wody
     tw_input.grid(column=2, row=count)
+
     # zasolenie
     zsl_input.grid(column=3, row=count)
 
@@ -49,16 +50,16 @@ def counting():
 
 def btncalc():
     print(count)
+    # Fajnie do tych co ustawiam w głownym kodzie mogę się dostać
     tw1 = float(tw_1Input.get())
     zsl1 = float(zsl_1input.get())
     print(tw1, zsl1)
 
 
-
-
 # wazne stale
 count = 2
-#listy
+
+# listy fajnie by bylo jakby mozna bylo kolejne pola do wpisu wrzucac tutaj
 l_tw = []
 l_zsl = []
 
@@ -69,6 +70,8 @@ root.title('Kalkulator zanurzeń')
 # top lable
 tw_1Label = tkinter.Label(root, text='Temperatura wody')
 zsl_1Lable = tkinter.Label(root, text='Zasolenie')
+g_wody1Lable = tkinter.Label(root, text='Gęstość wody')
+z_statku1Lable = tkinter.Label(root, text='Zanurzenie statku')
 
 # first side lable
 akwnLable = tkinter.Label(root, text='Akwen 1')
@@ -78,21 +81,25 @@ tw_1Input = tkinter.Entry(root, )
 zsl_1input = tkinter.Entry(root, )
 
 # guzik dodania więcej akwenów
-btnad = tkinter.Button(root, text='Dodaj akwen.', padx=20, command=btnclck)
+btn_add = tkinter.Button(root, text='Dodaj akwen.', padx=20, command=btnpress)
 
 # guzik kalkuluj
 btncalculate = tkinter.Button(root, text='Oblicz', padx=20, command=btncalc)
 
 # packing
+# input fields
+tw_1Input.grid(column=2, row=2)
+zsl_1input.grid(column=3, row=2)
+# lable
 akwnLable.grid(column=1, row=2)
 tw_1Label.grid(column=2, row=1)
-tw_1Input.grid(column=2, row=2)
 zsl_1Lable.grid(column=3, row=1)
-zsl_1input.grid(column=3, row=2)
-btnad.grid(column=1, row=50)
-btncalculate.grid(column=4, row=50)
+g_wody1Lable.grid(column=4, row=1)
+z_statku1Lable.grid(column=5, row=1)
 
-
+# guziki
+btn_add.grid(column=1, row=1)
+btncalculate.grid(column=4, columnspan=2, ipadx=50, row=50)
 
 # petla
 root.mainloop()
--- tKinter.py
+++ tKinter.py
@@ -1,9 +1,28 @@
-import tkinter as tk
-from tkinter import ttk
+import tkinter
+from tkinter import *
 
 
-des_q1 = ttk.Label(text='Proszę o podanie ilości akwenów, na której znajdzie się statek.')
+def callback(u_input):
+    if u_input.isdigit():
+        print(u_input)
+        return True
+
+    elif u_input is "":
+        print(u_input)
+        return True
+
+    else:
+        print(u_input)
+        return False
 
 
-root = tk.Tk()
+root = Tk()
+
+e = Entry(root)
+e.place(x=50, y=50)
+reg = root.register(callback)
+
+e.config(validate="key",
+         validatecommand=(reg, '% P'))
+
 root.mainloop()
