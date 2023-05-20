ac4593cdb733544a5263d9fda50bfd4cb0849b4e
FilipoPoland <filipopoland@wp.pl>
2023-05-20 14:51:19 +0200

    Commit

--- Akweny_gui_dictionaries.py
+++ Akweny_gui_dictionaries.py
@@ -23,23 +23,33 @@ import tkinter
 #     return fr_cptr
 
 def btnpress():
+    global pola_tw
+    global pola_zsl
     counting()
 
+    key = f'num{count-1}'   # tworzymy klucz do słownika i przypisujemy mu wartość string z dynamiczną końcówką
+    pola_tw[key] = tkinter.Entry(root, validate='key')      # tworzymy wpis w słowniku - klucz (którego nazwę tworzymy wyżej dynamicznie) używamy jak zmienną i przypisujemy do niego odpowiednią wartość
+    pola_zsl[key] = tkinter.Entry(root, )   # tak samo dla zasolenia
     # to mimo dodawania pól nie robi dla mnie nic przydatnego bo nie wiem jak odwołać się do tych pół
     # akwen nazwa
     lbl = tkinter.Label(text=f'Akwen {count-1}')
     # temperatura wody pole do danych
-    tw_input = tkinter.Entry(root, validate='key')
+    # tw_input = tkinter.Entry(root, validate='key')
     # zasolenie pole danych
-    zsl_input = tkinter.Entry(root, )
+    # zsl_input = tkinter.Entry(root, )
 
     # akwen nazwa
     lbl.grid(column=1, row=count)
     # temperatura wody
-    tw_input.grid(column=2, row=count)
+    # tw_input.grid(column=2, row=count)
+    pola_tw[key].grid(column=2, row=count)
 
     # zasolenie
-    zsl_input.grid(column=3, row=count)
+    # zsl_input.grid(column=3, row=count)
+    pola_zsl[key].grid(column=3, row=count)
+    print(pola_tw)  # sprawdzenie, czy słownik zapisuje się poprawnie
+    print(pola_zsl) # to samo
+
 
 
 def counting():
@@ -49,12 +59,21 @@ def counting():
 
 
 def btncalc():
+    global pola_tw
+    global pola_zsl
+    global l_tw   
+    global l_zsl
     print(count)
-
     # Fajnie do tych co ustawiam w głownym kodzie mogę się dostać
     tw1 = float(tw_1Input.get())
     zsl1 = float(zsl_1input.get())
     print(tw1, zsl1)
+    for i in pola_tw.values():
+        l_tw.append(float(i.get()))
+    for i in pola_zsl.values():
+        l_zsl.append(float(i.get()))
+    print(l_tw)
+    print(l_zsl)
 
 
 # wazne stale
@@ -63,6 +82,8 @@ count = 2
 # listy fajnie by bylo jakby mozna bylo kolejne pola do wpisu wrzucac tutaj
 l_tw = []
 l_zsl = []
+pola_tw = {}        # słownik na pola temperatury
+pola_zsl = {}       # słownik na pola zasolenia
 
 # program
 root = tkinter.Tk()
@@ -71,8 +92,6 @@ root.title('Kalkulator zanurzeń')
 # top lable
 tw_1Label = tkinter.Label(root, text='Temperatura wody')
 zsl_1Lable = tkinter.Label(root, text='Zasolenie')
-g_wody1Lable = tkinter.Label(root, text='Gęstość wody')
-z_statku1Lable = tkinter.Label(root, text='Zanurzenie statku')
 
 # first side lable
 akwnLable = tkinter.Label(root, text='Akwen 1')
@@ -88,19 +107,15 @@ btn_add = tkinter.Button(root, text='Dodaj akwen.', padx=20, command=btnpress)
 btncalculate = tkinter.Button(root, text='Oblicz', padx=20, command=btncalc)
 
 # packing
-# input fields
-tw_1Input.grid(column=2, row=2)
-zsl_1input.grid(column=3, row=2)
-# lable
 akwnLable.grid(column=1, row=2)
 tw_1Label.grid(column=2, row=1)
+tw_1Input.grid(column=2, row=2)
 zsl_1Lable.grid(column=3, row=1)
-g_wody1Lable.grid(column=4, row=1)
-z_statku1Lable.grid(column=5, row=1)
+zsl_1input.grid(column=3, row=2)
+btn_add.grid(column=1, row=50)
+btncalculate.grid(column=4, row=50)
 
-# guziki
-btn_add.grid(column=1, row=1)
-btncalculate.grid(column=4, columnspan=2, ipadx=50, row=50)
+
 
 # petla
 root.mainloop()
